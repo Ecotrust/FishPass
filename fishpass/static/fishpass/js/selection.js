9c44c9c6db1bed80d6c9950bc3aad3381be7e0bf
David Pollard <pollardld@gmail.com>
2018-10-11 14:13:27 -0700

    Add select county

--- fishpass/static/fishpass/js/selection.js
+++ fishpass/static/fishpass/js/selection.js
@@ -5,9 +5,8 @@ app.map.selection = {
     app.map.selection.select = selectInteraction;
     app.map.addInteraction(app.map.selection.select);
     app.map.selection.select.on('select', function(event) {
-      console.log('selection event at ' + ol.coordinate.toStringHDMS(ol.proj.transform(
-      event.mapBrowserEvent.coordinate, 'EPSG:3857', 'EPSG:4326'
-    )));
+      console.log('selection event at ' + ol.coordinate.toStringHDMS(ol.proj.transform(event.mapBrowserEvent.coordinate, 'EPSG:3857', 'EPSG:4326')));
+    });
     app.map.selection.select.getFeatures().forEach(function(feat) {
         var layer = app.map.selection.select.getLayer(feat).get('id');
         app.map.layer[layer].selectAction(feat);
@@ -22,7 +21,7 @@ app.map.interaction = {
   selectFilter: new ol.interaction.Select({
     layers: [
       app.map.layer.huc10.layer,
-      app.map.layer.huc12.layer
+      app.map.layer.huc12.layer,
       app.map.layer.county.layer,
     ],
     style: app.map.styles.PolygonSelected
