352081c3ac38d3a5392c154337eccbff7ccb2a06
Ryan Hodges <ryan.d.hodges@gmail.com>
2018-12-12 15:23:33 -0800

    more tweaks to celery boot process

--- fishpass/deploy/celery_startup.sh
+++ fishpass/deploy/celery_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir /var/run/celery
-chmod celery:celery /var/run/celery
+chown celery:celery /var/run/celery
 ${CELERY_BIN} multi start ${CELERYD_NODES} -A ${CELERY_APP} --pidfile=${CELERYD_PID_FILE} --logfile=${CELERYD_LOG_FILE} --loglevel=${CELERYD_LOG_LEVEL} ${CELERYD_OPTS}
