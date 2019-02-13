e998f61e63bfdaed6c16d83f4669dcbe5524eaf7
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-02-13 09:51:12 -0800

    formatting budget range buttons

--- project_settings.py
+++ project_settings.py
@@ -178,6 +178,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.gis',
     'django.contrib.flatpages',
+    'django.contrib.humanize',
     'marineplanner',
     'core',
     'compressor',
