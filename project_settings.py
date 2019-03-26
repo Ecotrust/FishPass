9073920b7eeaf9f45f3b2f5f3e346cc84037267b
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-03-26 10:38:18 -0700

    weird bug on AWS may be related to this. Also can't enable sharing (not that we want to).

--- project_settings.py
+++ project_settings.py
@@ -14,6 +14,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from collections import OrderedDict
 
+SHARING_TO_PUBLIC_GROUPS=['Share with Public']
+SHARING_TO_STAFF_GROUPS=['Share with staff']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
