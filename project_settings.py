8a385e4bb417d6eaa50d8a0b32788d38c39ce628
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-04-01 16:45:16 -0700

    setting appropriate RTE fields for CKEditor

--- project_settings.py
+++ project_settings.py
@@ -206,6 +206,22 @@ INSTALLED_APPS = [
     'flatblocks',
 ]
 
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Format'],
+            ['Bold', 'Italic', 'Underline','Strike','Subscript','Superscript'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['Image','Table','HorizontalRule','SpecialChar'],
+            [ 'TextColor','BGColor' ],
+            ['Undo','Redo'],
+            ['RemoveFormat', 'Source']
+        ],
+    }
+}
+
 CELERY_RESULT_BACKEND = 'django-db'
 
 # GET_SCENARIOS_URL = '/get_scenarios'
